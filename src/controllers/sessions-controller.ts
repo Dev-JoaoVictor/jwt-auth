a4bff965cd04c4d7af1ea507871817bcf0aeaf5b
Dev-JoaoVictor <joaovictor.ramos34@gmail.com>
2025-04-20 14:44:34 -0300

    feat(extraindo o token): criandoo middleware de extracao do token no header da requisicao

--- src/controllers/sessions-controller.ts
+++ src/controllers/sessions-controller.ts
@@ -1,8 +1,30 @@
+import { AppError } from "@/utils/AppError";
 import { Request, Response } from "express";
+import { authConfig } from "@/configs/auth";
+import { sign } from "jsonwebtoken";
 
 class SessionsController {
   async create(request: Request, response: Response) {
-    return response.json({ message: "ok" });
+    const { username, password } = request.body;
+
+    const userFake = {
+      id: 1,
+      username: "Joao",
+      password: "1234",
+    };
+
+    if (username !== userFake.username || password !== userFake.password) {
+      throw new AppError("E-mail e/ou senha incorreto!", 401);
+    }
+
+    const { secret, expiresIn } = authConfig.jwt;
+
+    const token = sign({}, secret, {
+      expiresIn,
+      subject: String(userFake.id),
+    });
+
+    return response.json({ token });
   }
 }
 
